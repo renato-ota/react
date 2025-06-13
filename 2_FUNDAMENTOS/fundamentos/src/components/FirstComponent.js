1626f41a0e981f3dc8d75c32318e2546284df5df
planetun-otarenato <renato.ota@planetun.com.br>
2025-06-13 12:00:49 -0300

    commit inicial

---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+const FirstComponent = () => {
+
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+            <p>Este é o meu primeiro componente em React!</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
